0a8c5cf8059c71fe0430b2060c4dcd79f24bfd34
Wojciech Baron <wabaron98@gmail.com>
2025-05-08 19:51:21 +0200

    Add project files.

---
+++ QUIZWPFKLASA3/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace QUIZWPFKLASA3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Quiz> quizList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadQuiz();
+            ShowQuestion();
+        }
+        private void LoadQuiz()
+        {
+            string path = "Quiz.json";
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                quizList = JsonSerializer.Deserialize<List<Quiz>>(json);
+            }
+        }
+
+        private void ShowQuestion()
+        {
+          //tutaj ma być logika odpowiadająca za wyświetlanie pytania
+
+        }
+        private void CheckAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            //tutaj ma być logika, która sprawdza pytania i przydziela punkty
+        }
+        private void NextAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            //tutaj ma być logika, przenosi do kolejnego pytania
+        }
+    }
+}
---
+++ QUIZWPFKLASA3/Quiz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QUIZWPFKLASA3
+{
+    class Quiz
+    {
+        [JsonPropertyName("pytanie")]
+        public string Pytanie { get; set; }
+
+        [JsonPropertyName("odpowiedzi")]
+        public Dictionary<string, string> Odpowiedzi { get; set; }
+
+        [JsonPropertyName("poprawna")]
+        public string Poprawna { get; set; }
+    }
+}
